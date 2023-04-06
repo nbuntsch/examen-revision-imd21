e7430fadfc77229cd4469a2706cad31c3c3f9196
Lisa-str <Lisa.Staruch@studentfr.ch>
2023-04-06 09:30:53 +0200

    bonus js

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,10 @@
-const trigger = document.querySelector('.js-trigger');
-const target = document.querySelector('.js-target');
-const close = document.querySelector('.js-close');
+var containerLabel = document.querySelector(".container-label");
+var shrek = document.querySelector(".shrek");
+var minus = document.querySelectorAll(".burger-menu-minus");
+var plus = document.querySelectorAll(".burger-menu");
 
-trigger.addEventListener('click', () => {
-  target.classList.toggle('is-active');
-  trigger.classList.toggle('is-active');
+containerLabel.addEventListener("click", () => {
+  shrek.classList.toggle("is-active");
+  plus.classList.toggle("exchange");
+  minus.classList.toggle("is-active");
 });
-
-close.addEventListener('click', () => {
-  target.classList.toggle('is-active');
-  trigger.classList.toggle('is-active');
-});
-
-// const trigger2 = document.querySelector('.js-trigger-2');
-// const target2 = document.querySelector('.js-target-2');
-// trigger2.addEventListener('click', () => {
-//   target2.classList.toggle('is-active');
-// });
-
-// const trigger3 = document.querySelector('.js-trigger-3');
-// const target3 = document.querySelector('.js-target-3');
-// const target3c = document.querySelector('.js-target-3c');
-// trigger3.addEventListener('click', () => {
-//   target3.classList.toggle('is-active');
-//   target3c.classList.toggle('is-active');
-// });
