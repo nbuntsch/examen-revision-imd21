a218da15b972c7dbd2281f0056114f2832889a6d
darek vassalli <98399869+DarekV@users.noreply.github.com>
2023-04-06 09:00:54 +0200

    js fini

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-const trigger = document.querySelector('.js-trigger');
-const target = document.querySelector('.js-target');
-const close = document.querySelector('.js-close');
+// const trigger = document.querySelector('.js-trigger');
+// const target = document.querySelector('.js-target');
+// const close = document.querySelector('.js-close');
 
-trigger.addEventListener('click', () => {
-  target.classList.toggle('is-active');
-  trigger.classList.toggle('is-active');
-});
+// trigger.addEventListener('click', () => {
+//   target.classList.toggle('is-active');
+//   trigger.classList.toggle('is-active');
+// });
 
-close.addEventListener('click', () => {
-  target.classList.toggle('is-active');
-  trigger.classList.toggle('is-active');
-});
+// close.addEventListener('click', () => {
+//   target.classList.toggle('is-active');
+//   trigger.classList.toggle('is-active');
+// });
 
 // const trigger2 = document.querySelector('.js-trigger-2');
 // const target2 = document.querySelector('.js-target-2');
@@ -25,3 +25,15 @@ close.addEventListener('click', () => {
 //   target3.classList.toggle('is-active');
 //   target3c.classList.toggle('is-active');
 // });
+
+const trigger = document.querySelector(".trigger");
+const content = document.querySelector(".content");
+const plus = document.querySelector(".plus");
+const moin = document.querySelector(".moin");
+let openContent = function () {
+  plus.classList.toggle("activ");
+  moin.classList.toggle("activ");
+  content.classList.toggle("open");
+};
+
+trigger.addEventListener("click", openContent);
